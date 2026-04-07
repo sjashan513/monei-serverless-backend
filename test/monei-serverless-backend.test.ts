b19dc6c844517a9a4a76ea8eb6d1bac5cb6cf679
Jashanpreet Singh <jashanpreet.singh@cleverea.com>
2026-04-07 20:09:36 +0200

    Initial commit

---
+++ test/monei-serverless-backend.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MoneiServerlessBackend from '../lib/monei-serverless-backend-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/monei-serverless-backend-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MoneiServerlessBackend.MoneiServerlessBackendStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
